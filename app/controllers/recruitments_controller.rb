5bb4fe7d7073e8b4eb87dc18c3a1e70c1ea6d10b
大平勝也 <k8t2y9.cat@gmail.com>
2019-03-24 16:56:04 +0900

    feat: recruitmentリソースを追加

---
+++ app/controllers/recruitments_controller.rb
@@ -0,0 +1,51 @@
+class RecruitmentsController < ApplicationController
+  before_action :set_recruitment, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @recruitments = Recruitment.all
+  end
+
+  def show
+  end
+
+  def new
+    @recruitment = Recruitment.new
+  end
+
+  def create
+    @recruitment = Recruitment.new(recruitment_params)
+    @recruitment.save
+    redirect_to recruitments_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @recruitment.update(recruitment_params)
+      flash[:notice] = '投稿に成功しました'
+      redirect_to recruitments_path
+    else
+      render 'edit'
+      flash.now[:alert] = '投稿に失敗しました'
+    end
+  end
+
+  def destroy
+    @recruitment.destroy
+    flash.now[:alert] = '投稿を削除しました'
+    redirect_to recruitments_path
+  end
+
+
+
+
+  private
+    def recruitment_params
+      params.require(:recruitment).permit(:title, :content)
+    end
+
+    def set_recruitment
+      @recruitment = Recruitment.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   %w(about concept).each do |path|
     get path, to: "static_pages##{path}"
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :recruitments
 end
