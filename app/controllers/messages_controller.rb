45da0f377c5d3bf69eaae43cff65082a649e8a5e
大平勝也 <k8t2y9.cat@gmail.com>
2019-03-31 17:17:17 +0900

    feat: トークルームの作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
+
     if @message.save
       redirect_to group_messages_path(@group)
       flash[:notice] = 'メッセージが送信されました'
