a3f0a0f8284fbb82bf4aa0916c94811e37d792c8
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-03 12:20:52 +0900

    fix: groupの修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,14 +8,32 @@ class GroupsController < ApplicationController
       myGroupIds << c_user.group.id
     end
 
+    @anotherGroupUsers = GroupUser.where(group_id: myGroupIds).where(user_id: @user_id)
+
+    group = Group.find_by(id: params[:id])
+    @group_user = GroupUser.where.not(user_id: current_user.id).first.user
+    @messages = Message.where(group: group).order(:created_at)
+
   end
 
 
   def create
-    @group = Group.create(group_params)
+    current_user_groups = GroupUser.where(user_id: current_user.id)
+    group = GroupUser.where(group: current_user_groups, user_id: params[:id])
+    if group.present?
+      redirect_to groups_path
+    else
+      group = Group.create(group_params)
+      GroupUser.create(group: group, user_id: current_user.id)
+      GroupUser.create(group: group, user_id: params[:user_id])
+      redirect_to group_messages_path(@group)
+    end
+
   end
 
   private
   def group_params
+    params.require(:group).permit(:id, user_ids: [])
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  # before_action :set_group, only: :show
 
   def index
     @users = User.all
@@ -8,4 +9,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @recruitments = Recruitment.where(user_id: @user_id)
   end
+
+  # private
+  # def set_group
+  #   @group = Group.find(params[:id])
+  # end
 end
