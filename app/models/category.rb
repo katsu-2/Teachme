4bcd1e4cfbede1e8108644e893b2d2bb035b45db
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-21 16:05:13 +0900

    feat: カテゴリーをenumを使って表示

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   has_many :recruitment_categories
   has_many :recruitments, through: :recruitment_categories
+
+  enum category: {
+    IT関係: 1, スポーツ: 2, ファッション: 3, 恋愛: 4, 仕事関係: 5, 勉強: 6, ライフワーク: 7, 手芸: 8, その他: 9
+  }
 end
