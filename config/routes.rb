0f8e58b6f2581ebd738bb257ad6c0e9fbdbdb2c5
大平勝也 <k8t2y9.cat@gmail.com>
2019-06-01 14:50:52 +0900

    fix: 不要なファイルの削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   get 'teacher/index'
   devise_for :users
   root 'recruitments#index'
-  %w(about concept).each do |path|
-    get path, to: "static_pages##{path}"
-  end
+
   resources :users, only: %i(index show)
   resources :recruitments
   resources :groups do
