f33792f5992f07cf8292028b60c5a510fd36f51c
大平勝也 <k8t2y9.cat@gmail.com>
2019-06-04 14:29:59 +0900

    fix: n+1の解消と文字数制限

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -1,6 +1,6 @@
 class TeacherController < ApplicationController
   def index
-    @users = User.where("person_type = 1")
+    @users = User.where("person_type = 1").includes(:recruitments)
     @teachers = @users.map { |user| user }
   end
 end
